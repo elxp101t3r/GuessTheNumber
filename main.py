66f3dff2907867d9100754eead1929fdc9668c10
elxp101t3r <eliotx10101@outlook.com>
2023-10-29 16:46:15 +0200

    Compare function

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from art import logo
 import random as r
 numbers = [x for x in range(1,100)]
 cpu_choice = r.choice(numbers)
+def compare(cpu,user):
+    if user == cpu:
+        return "You guess it!"
+    if user < cpu:
+        return "Too low!"
+    if user > cpu:
+        return "Too high!"
