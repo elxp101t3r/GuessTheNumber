cd18faa0ece49e45067d8471038ac9fbd59a88ee
elxp101t3r <eliotx10101@outlook.com>
2023-10-29 16:51:32 +0200

    easy level

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ p_game = True
 easy = 10
 hard = 5
 level = input('easy or hard: ')
+if level == 'easy':
+    os.system('clear')
+    print(logo)
+    print(f'You got {easy} chances...')
+    while p_game:
+        user_choice = int(input('Guess the number between 1 and 100: '))
+        if compare(cpu_choice, user_choice) == 'You guess it!':
+            p_game = False
+        os.system('clear')
+        print(logo)
+        print(compare(cpu_choice, user_choice))
+        easy -= 1
+        if easy == 0:
+            print('You lose all the chances..')
+            break
